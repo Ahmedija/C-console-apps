f783d666b6b1004979c350e1fab4f9e3c85ff9b8
Ahmedija <36454914+Ahmedija@users.noreply.github.com>
2019-09-19 07:14:25 -0700

    Add files via upload

---
+++ Fibonacci console app.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+
+            bool Valid = false;
+            int Number;
+
+
+            while (Valid == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to fibonacci");
+                Console.WriteLine("Please enter number of iterations: \n");
+                string Input = Console.ReadLine();
+                if (int.TryParse(Input, out Number))
+                {
+           
+                    Valid = true;
+                    for (int i = 0; i < Number; i++)
+                    {
+                        Console.Write(Fibonacci(i) + ", ");
+           
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nNot an integer, please try again.");
+                }
+
+                Console.WriteLine("\n\nPress enter");
+                Console.ReadLine();
+            }
+            
+
+
+        }
+    }
+}
+
+
